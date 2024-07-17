481b7171a0238e8b6370d619e61b6a59e3d53976
orangeguppy <vuwet467@gmail.com>
2024-07-17 23:02:13 +0800

    Test.cs

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello");
+        StartCoroutine(GetRequest("http://localhost:8000/"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError(webRequest.error);
+        }
+        else
+        {
+            Debug.Log(webRequest.downloadHandler.text);
+        }
+    }
+}
