4fc4e4f0b32429d030b1a7c14cc6e17e7dd5444a
samxddICANT <samuel.goh.2021@scis.smu.edu.sg>
2024-11-25 22:08:07 +0800

    teams/dept input

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class MainMenuManager : MonoBehaviour
     public GameObject accountSettings;
     public GameObject sendOTP;
     public GameObject resetPwPage;
+    public GameObject teamsScreen;
+    public GameObject departmentScreen;
+
+    public TMP_InputField teamInput;
+    public TMP_InputField deptInput;
 
     public void ShowScreen(GameObject toEnable, GameObject toDisable)
     {
@@ -25,5 +31,45 @@ public class MainMenuManager : MonoBehaviour
     public void ShowPWResetScreen()
     {
         ShowScreen(resetPwPage, sendOTP);
+    }
+
+    public void ShowTeamsScreen()
+    {
+        ShowScreen(teamsScreen, accountSettings);
+    }
+
+    public void ShowDepartmentScreen()
+    {
+        ShowScreen(departmentScreen, accountSettings);
+    }
+
+    public void JoinTeam()
+    {
+        string teamName = teamInput.text; 
+
+        if (!string.IsNullOrEmpty(teamName))
+        {
+            PlayerPrefs.SetString("Team", teamName); 
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.LogWarning("Team empty");
+        }
+    }
+
+    public void JoinDept()
+    {
+        string deptName = deptInput.text;
+
+        if (!string.IsNullOrEmpty(deptName))
+        {
+            PlayerPrefs.SetString("Department", deptName);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.LogWarning("Dept empty");
+        }
     }
 }
